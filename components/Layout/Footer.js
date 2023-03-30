a7adb2f59cc86d740ae63ef98a60668b5272eacd
Carlo Denaro <818568+blackout314@users.noreply.github.com>
2023-03-30 17:31:41 +0200

    logo footer

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import LogoVPN from "../../public/assets/Logo.svg";
+import Image from "next/image";
 import Facebook from "../../public/assets/Icon/facebook.svg";
 import Twitter from "../../public/assets/Icon/twitter.svg";
 import Instagram from "../../public/assets/Icon/instagram.svg";
@@ -8,7 +9,14 @@ const Footer = () => {
     <div className="bg-white-300 pt-44 pb-24">
       <div className="max-w-screen-xl w-full mx-auto px-6 sm:px-8 lg:px-16 grid grid-rows-6 sm:grid-rows-1 grid-flow-row sm:grid-flow-col grid-cols-3 sm:grid-cols-12 gap-4">
         <div className="row-span-2 sm:col-span-4 col-start-1 col-end-4 sm:col-end-5 flex flex-col items-start ">
-          <LogoVPN className="h-8 w-auto mb-6" />
+          <Image
+            src="/assets/studioticino.png"
+            alt="Aiuto Scolastico Ticino"
+            quality={100}
+            width={128}
+            height={32}
+            layout="responsive"
+          />
           <p className="mb-4">
             <strong className="font-medium">studioticino.com</strong> Offriamo servizi di aiuto scolastico, tutoring online e lezioni private per studenti delle scuole superiori nel Ticino.
           </p>
