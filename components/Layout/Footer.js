cd1fad7d8adff4e41b5be5b59c8d22fb41e172d1
Carlo Denaro <818568+blackout314@users.noreply.github.com>
2023-03-30 16:40:13 +0200

    Update Footer.js

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import LogoVPN from "../../public/assets/studioticino.png";
+import LogoVPN from "../../public/assets/Logo.svg";
 import Facebook from "../../public/assets/Icon/facebook.svg";
 import Twitter from "../../public/assets/Icon/twitter.svg";
 import Instagram from "../../public/assets/Icon/instagram.svg";
